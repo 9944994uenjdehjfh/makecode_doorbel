8e4a6ac927e29692d8c88348ed09521cb4fa292c
9944994uenjdehjfh <koningmuis1+gh2@gmail.com>
2025-03-17 09:19:42 +0100

    Push edits

--- main.ts
+++ main.ts
@@ -6,19 +6,20 @@ grove.onGesture(GroveGesture.Right, function () {
 function israng () {
     music.play(music.tonePlayable(349, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
     if (on == 0) {
-        for (let index = 0; index < 8; index++) {
-            if (input.buttonIsPressed(Button.A)) {
+        for (let index = 0; index < 40; index++) {
+            if (input.buttonIsPressed(Button.A) || grove.measureInCentimetersV2(DigitalPin.P2) <= 5) {
                 on = 1
                 strip.showColor(neopixel.colors(NeoPixelColors.Black))
                 strip.show()
             } else {
-                music.play(music.tonePlayable(349, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+                music.play(music.tonePlayable(9999, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
                 strip.showColor(neopixel.colors(NeoPixelColors.Red))
                 strip.show()
                 basic.pause(1000)
                 strip.showColor(neopixel.colors(NeoPixelColors.White))
                 strip.show()
                 basic.pause(1000)
+                Time_door += 1
             }
         }
     } else {
@@ -30,6 +31,7 @@ input.onButtonPressed(Button.B, function () {
     on = 0
     israng()
 })
+let Time_door = 0
 let strip: neopixel.Strip = null
 let on = 0
 let amount = 0
